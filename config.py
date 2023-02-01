f70a8d24950ac98247e97554f0fdab47f1de593b
Roma <99729184+romaazarubin@users.noreply.github.com>
2023-02-01 22:14:44 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-bot_token = '5729797343:AAGiDN6wWnAenupfhs6zxHzHhoGVamE2uYI'
-support_id = [5261983952]
-ip = 'localhost'
-PG_USER = 
-PG_PASSWORD = 
-DATABASE = 
-
-POSTGRES_URL = f'postgresql://{PG_USER}:{PG_PASSWORD}@{ip}/{DATABASE}'
